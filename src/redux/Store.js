c06dccd97c52190fcc227c10c3e6e39aee6bbe53
Jean Bulambo <jeanbulambo4@gmail.com>
2022-08-23 00:37:56 +0200

    Update store by configurationStore method

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,8 +1,12 @@
-import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './Books/Books';
 import CategoryReducer from './category/category';
 
-export default combineReducers({
-  booksReducer,
-  CategoryReducer,
+const store = configureStore({
+  reducer: {
+    Books: booksReducer,
+    category: CategoryReducer,
+  },
 });
+
+export default store;
